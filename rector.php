7ef3f41a2183bc80b7b3d4bb5769685fb4ddbbc9
NicolasKion <172045106+NicolasKion@users.noreply.github.com>
2025-01-17 11:09:35 +0000

    Fix styling

--- rector.php
+++ rector.php
@@ -8,13 +8,13 @@ use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 return RectorConfig::configure()
     ->withRules(
         [
-            DeclareStrictTypesRector::class
+            DeclareStrictTypesRector::class,
         ]
     )
     ->withPaths([
-        __DIR__ . '/config',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__.'/config',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ])
     // uncomment to reach your current PHP version
     // ->withPhpSets()
