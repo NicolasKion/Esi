f7852bb3d7e3aac5a2188ae52ac9d680b81590f9
Tim Kunze <contact@timkunze.at>
2026-05-19 22:03:44 +0200

    chore: bump ESI compatibility date to 2026-05-19

--- config/esi.php
+++ config/esi.php
@@ -3,16 +3,16 @@
 declare(strict_types=1);
 
 return [
-    /** @phpstan-ignore larastan.noEnvCallsOutsideOfConfig  */
+    /** @phpstan-ignore larastan.noEnvCallsOutsideOfConfig */
     'user_agent' => env('APP_NAME'),
-    /** @phpstan-ignore larastan.noEnvCallsOutsideOfConfig  */
+    /** @phpstan-ignore larastan.noEnvCallsOutsideOfConfig */
     'client_id' => env('EVE_CLIENT_ID'),
-    /** @phpstan-ignore larastan.noEnvCallsOutsideOfConfig  */
+    /** @phpstan-ignore larastan.noEnvCallsOutsideOfConfig */
     'client_secret' => env('EVE_CLIENT_SECRET'),
     'retry_policy' => [
         'tries' => 5,
         'delay' => 5000,
     ],
     'base_url' => 'https://esi.evetech.net',
-    'compatibility_date' => '2025-08-26',
+    'compatibility_date' => '2026-05-19',
 ];
